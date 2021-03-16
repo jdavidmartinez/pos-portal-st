4126343f7e56f76809bbe4f9b5a95dc1b2aa3788
Yovanny López <yovannyinstructor@gmail.com>
2021-03-16 06:26:59 -0500

    infra: added admin template [#TK-39]

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/* eslint-disable global-require */
+import './assets/css/vendor/bootstrap.min.css';
+import './assets/css/vendor/bootstrap.rtl.only.min.css';
+import 'react-circular-progressbar/dist/styles.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-image-lightbox/style.css';
+import 'video.js/dist/video-js.css';
+import {
+  isMultiColorActive,
+  defaultColor,
+  isDarkSwitchActive,
+} from './constants/defaultValues';
+import { getCurrentColor, setCurrentColor } from './helpers/Utils';
 
-import './index.css';
-import App from './components/Home/App';
-import reportWebVitals from './reportWebVitals';
+const color =
+  isMultiColorActive || isDarkSwitchActive ? getCurrentColor() : defaultColor;
+setCurrentColor(color);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
+const render = () => {
+  import(`./assets/css/sass/themes/gogo.${color}.scss`).then(() => {
+    require('./AppRenderer');
+  });
+};
+render();
