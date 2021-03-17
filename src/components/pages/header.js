c917a6c153d46b2d70f8bd5de02303b86a6ba793
DaviD D <jdavid.martinez@gmail.com>
2021-03-17 11:43:58 -0500

    infra:still styling header page

--- src/components/pages/header.js
+++ src/components/pages/header.js
@@ -1,22 +1,21 @@
-import logo from '../../assets/logos/P1-10.png';
-import Hero from '../../assets/img/img_1_all.png';
+import logo from "../../assets/logos/P1-10.png";
+import Hero from "../../assets/img/img_1_all.png";
 
-
-import '../../assets/css/header.css'
+import "../../assets/css/header.css";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div>
-            <img src={logo} className="Header-logo" alt="logo" />  
-            <button className="ingressButton">Ingresar</button> 
-        </div>
-        
-        <img src={Hero} className="Hero" alt="Hero" />
-
-      
+  return (
+    <div className="header">
+      <div className="headerSection">
+        <img src={logo} className="Header-logo" alt="logo" />
+        <button className="ingressButton">Ingresar</button>
       </div>
-    );
-  };
+      <div>
+        <p className="creamos">Creamos y Evaluamos tu Hoja de Vida</p>
+        <img src={Hero} className="Hero" alt="Hero" />
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
