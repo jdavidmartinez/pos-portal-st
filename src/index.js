380a12c070f547da85b70f216aed2745fecd43a4
DaviD D <jdavid.martinez@gmail.com>
2021-03-17 10:15:19 -0500

    infra: basic configuration for landing page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/css/index.css';
+<<<<<<< HEAD
+=======
+//import 'bootstrap/dist/css/bootstrap.min.css';
+>>>>>>> infra: basic configuration for landing page
 
 import App from './components/pages/App';
 import reportWebVitals from './reportWebVitals';
