c941b0bce8dff853c3b12b41f58183dbb658fbd7
DaviD D <jdavid.martinez@gmail.com>
2021-03-18 16:11:27 -0500

    infra: creatin a card moudle called Card.js

---
+++ src/components/IU/Cards/Card.js
@@ -0,0 +1,19 @@
+import { Card } from "react-bootstrap";
+
+const cardType = () => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit, quis nullam
+          hendrerit orci at penatibus potenti, cursus condimentum porta risus
+          morbi sollicitudin. Auctor himenaeos fames egestas commodo non ligula
+          tempor class, feugiat cum dignissim arcu bibendum est convallis
+          facilisis
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+export default cardType;
