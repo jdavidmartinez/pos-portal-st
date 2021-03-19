f311f614fcab842e8c96e60d1271c1bef773682e
mayerly386811 <mayerly386811@gmail.com>
2021-03-19 18:28:56 -0500

    imagenes en las cajas

--- src/components/layouts/Features.jsx
+++ src/components/layouts/Features.jsx
@@ -1,5 +1,6 @@
-import Title from '../UI/Titles/Title';
 import Box from '../UI/Box/Box';
+import Title from '../UI/Titles/Title';
+
 import identify from '../../assets/img/Identify.png';
 import acompañamiento from '../../assets/img/Acompañamiento.png';
 import conexion from '../../assets/img/Conexion.png';
@@ -10,19 +11,19 @@ export default function Features() {
       <Title title="¿Por qué nosotros?" />
       <div className="containerFeature">
         <Box
-          src={identify}
+          imagen={identify}
           alt="Identify"
           title="Identificamos"
           description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
         />
         <Box
-          src={acompañamiento}
+          imagen={acompañamiento}
           alt="Acompañamiento"
           title="Acompañamiento"
           description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
         />
         <Box
-          src={conexion}
+          imagen={conexion}
           alt="Conexion"
           title="Conectamos"
           description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum"
