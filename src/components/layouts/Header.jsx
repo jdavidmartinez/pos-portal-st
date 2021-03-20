9ee457e11d9ac4a495c8f36a99ed05deb6cc8a28
Yovanny López <yovannyinstructor@gmail.com>
2021-03-20 12:52:27 -0500

    feat: header-content-info styles [#TK-97]

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -10,20 +10,34 @@ const Header = () => (
     <Row className="justify-content-center">
       <Col md={10} className="header-top-bar">
         <img src={logo} alt="logo" className="header-logo" />
-        <div className="header-main-button">Ingresar</div>
+        <div className="header-ingress-button">Ingresar</div>
       </Col>
     </Row>
     <Row className="justify-content-center">
-      <Col xs={10} md={5}>
-        <h1 className="cv-text">Creamos y Evaluamos tu Hoja de Vida</h1>
-        <p>
-          Creación de hoja de vida, guía para llegar al empleo
-          deseado y capacitación para la entrevista de trabajo.
-        </p>
-        <div className="header-main-button">Evaluar HV</div>
+      <Col md={4}>
+        <Row className="header-info">
+          <Col md={9}>
+            <h1 className="header-content-title">Creamos y</h1>
+            <h1 className="header-content-title">evaluamos tu</h1>
+            <h1 className="header-content-title">Hoja de Vida</h1>
+          </Col>
+          <Col md={9}>
+            <p className="header-content-text">
+              Creación de hoja de vida, guía para llegar al empleo
+              deseado y capacitación para la entrevista de trabajo.
+            </p>
+          </Col>
+          <Col md={9}>
+            <div className="header-evaluate-hv-button">Evaluar HV </div>
+          </Col>
+        </Row>
+
       </Col>
-      <Col xs={10} md={5}>
+      <Col md={5}>
         <img src={hero} alt="hero" className="hero" />
+      </Col>
+      <Col md={1}>
+        whats
       </Col>
     </Row>
   </header>
