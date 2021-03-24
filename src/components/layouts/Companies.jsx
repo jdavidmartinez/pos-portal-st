af33335289db0b002bd49a2ba5c99462ddf80170
DaviD D <jdavid.martinez@gmail.com>
2021-03-24 09:51:21 -0500

    Infra: adding images to Partners card, lasks styles[TKT 101]

--- src/components/layouts/Companies.jsx
+++ src/components/layouts/Companies.jsx
@@ -1,25 +1,36 @@
 import { Col, Row } from 'react-bootstrap';
 
 import CardCompanies from '../UI/Cards/CardCompanies';
-
+import CardBrandsCompanies from '../UI/Cards/CardBrandsCompanies';
 import companiesImg from '../../assets/img/companies.png';
+import avtImg from '../../assets/img/avt_Ingenieria.png';
+import mesonImg from '../../assets/img/hotel_meson.png';
+import marquezImg from '../../assets/img/marquez_Fajardo.png';
+import camaraComercioImg from '../../assets/img/camara_comercio_armenia.png';
+import fondeEmprenderImg from '../../assets/img/fondo_emprender.png';
+import mirusImg from '../../assets/img/miruss_mart_solutions.png';
+import parqueSoftImg from '../../assets/img/PS_Corp.png';
+
 import '../../assets/css/companies.css';
 
 const Companies = () => (
   <section>
-    <Row>
-      <Col xs={10}>
-        <h1 className="services-title">Empresas</h1>
-      </Col>
-    </Row>
-    <Row className="cardStyle">
-      <Col xs={10} md={4} className="profileCard py-5">
-        <CardCompanies headerImage={companiesImg} title="Perfil laboral" />
-      </Col>
-      <Col xs={10} md={4} className="profileCard py-5">
-        <CardCompanies headerImage={companiesImg} title="Perfil laboral" />
-      </Col>
-    </Row>
+    <div style={{ display: 'block' }}>
+      <Row className="CompanieCard">
+        <Col xs={10} md={9} className="">
+          <CardBrandsCompanies
+            allyImage={camaraComercioImg}
+            allyImageTwo={fondeEmprenderImg}
+            allyImageThird={mirusImg}
+            allyImageFour={parqueSoftImg}
+            customImg={avtImg}
+            customImgtwo={mesonImg}
+            customImgThird={marquezImg}
+          />
+          <CardCompanies headerImage={companiesImg} />
+        </Col>
+      </Row>
+    </div>
   </section>
 );
 
