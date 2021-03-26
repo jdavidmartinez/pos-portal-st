28bac00b9633d5a3cd2b1a8ca10ee07c7cf3a1f2
Yovanny López <yovannyinstructor@gmail.com>
2021-03-26 17:43:58 -0500

    feat: AboutUs component styles [#TK-97]

--- src/components/layouts/AboutUs/AboutUs.jsx
+++ src/components/layouts/AboutUs/AboutUs.jsx
@@ -4,24 +4,29 @@ import './about-us.css';
 
 const AboutUs = () => (
   <>
-    <section className="row">
-      <h2 className="col-xs-12 h2-titles about-us-title">¿Quiénes somos?</h2>
-      <div className="col-xs-12 about-us">
-        <div className="col-xs-4">
-          <p className="about-us-text">
-            EmpleApp es una empresa que Conecta perfiles potenciales de una manera rapida,
-            ágil y efectiva, enfocando su metodología en las personas, potencializando el
-            perfil laboral e incrementando la visibilidad ante los reclutadores/buscadores
-            de empleo y facilita para las empresas la consecución de personal idóneo en menor
-            tiempo, mediante una plataforma web la cual permite agilizar y optimizar los procesos
-            de perfilación y reclutamiento impactando a mayor número de personas y empresas.
-          </p>
-          <p className="about-us-text">
-            Dicha metodología se realiza mediante un ecosistema que potencializa el perfil laboral
-            de los candidatos, acompaña, capacita, entrega información de valor para facilitar la
-            toma de decisiones y apoya en la fidelización del recurso humano, realizando un proceso
-            de selección de talento humano centrado en las personas.
-          </p>
+    <section>
+      <div className="row">
+        <h2 className="col-xs-12 h2-titles about-us-title">¿Quiénes somos?</h2>
+      </div>
+
+      <div className="row center-xs">
+        <div className="row about-us">
+          <div className="col-xs-5">
+            <p className="about-us-text">
+              EmpleApp es una empresa que Conecta perfiles potenciales de una manera rapida,
+              ágil y efectiva, enfocando su metodología en las personas, potencializando el
+              perfil laboral e incrementando la visibilidad ante los reclutadores/buscadores
+              de empleo y facilita para las empresas la consecución de personal idóneo en menor
+              tiempo, mediante una plataforma web la cual permite agilizar y optimizar los procesos
+              de perfilación y reclutamiento impactando a mayor número de personas y empresas.
+            </p>
+            <p className="about-us-text">
+              Dicha metodología se realiza mediante un ecosistema que potencializa el perfil laboral
+              de los candidatos, acompaña, capacita, entrega información de valor para facilitar la
+              toma de decisiones y apoya en la fidelización del recurso humano, realizando un proceso
+              de selección de talento humano centrado en las personas.
+            </p>
+          </div>
         </div>
       </div>
     </section>
