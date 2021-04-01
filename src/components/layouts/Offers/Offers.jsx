5eaed371fb3f88be6b37cacf24150047174ae5a9
DaviD D <jdavid.martinez@gmail.com>
2021-04-01 18:08:56 -0500

    Infra: layaout Offers component, traying to stablish 2 area i one secction

--- src/components/layouts/Offers/Offers.jsx
+++ src/components/layouts/Offers/Offers.jsx
@@ -1,15 +1,33 @@
-import { Carousel } from 'react-bootstrap';
+// import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './Offers.css';
+import './offers.css';
 
-import image1 from '../../../assets/img/companies/parquesoft.png';
-import image2 from '../../../assets/img/companies/camara-comercio.png';
+// import image1 from '../../../assets/img/companies/parquesoft.png';
+// import image2 from '../../../assets/img/companies/camara-comercio.png';
 
 const Offers = () => (
   <>
     <section>
-      <div className="row center-xs">
+      <div className="row center-xs offers">
+        <div className="row center-xs offers-title">
+          <h2 className="col-xs-12 offers-h2">Boletín</h2>
+        </div>
+        <div className="row center-xs offers-carousel-box">
+          <div className="col-xs-6">
+            <h2>carrusel</h2>
+          </div>
+          <div className="col-xs-6">
+            <h2>emergente</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
+);
+export default Offers;
+
+/*  <div className="row center-xs">
         <h1>Ofertas</h1>
         <div className="col-xs-12">
           <Carousel className="carousel vert slide">
@@ -21,8 +39,4 @@ const Offers = () => (
             </Carousel.Item>
           </Carousel>
         </div>
-      </div>
-    </section>
-  </>
-);
-export default Offers;
+      </div> */
