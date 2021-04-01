760f91723c4ba57a8a9cc37e2b64847c9cd22987
DaviD D <jdavid.martinez@gmail.com>
2021-03-31 22:22:51 -0500

    infra: genereting hirozontal carousel[TK104]

--- src/components/layouts/Offers/Offers.jsx
+++ src/components/layouts/Offers/Offers.jsx
@@ -7,18 +7,22 @@ import image1 from '../../../assets/img/companies/parquesoft.png';
 import image2 from '../../../assets/img/companies/camara-comercio.png';
 
 const Offers = () => (
-  <section>
-    <div>
-      <h1>CAROUSEL</h1>
-      <Carousel>
-        <Carousel.Item>
-          <img src={image1} alt="" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={image2} alt="" />
-        </Carousel.Item>
-      </Carousel>
-    </div>
-  </section>
+  <>
+    <section>
+      <div className="row center-xs">
+        <h1>Ofertas</h1>
+        <div className="col-xs-12">
+          <Carousel className="carousel vert slide">
+            <Carousel.Item className="carousel-item">
+              <img src={image1} alt="" />
+            </Carousel.Item>
+            <Carousel.Item className="carousel-item">
+              <img src={image2} alt="" />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </div>
+    </section>
+  </>
 );
 export default Offers;
