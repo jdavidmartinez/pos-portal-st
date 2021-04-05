7d94fe821410f76df06e6fc6dcebbf17a4f88d6e
DaviD D <jdavid.martinez@gmail.com>
2021-04-05 10:53:20 -0500

    Infra: creating Vertical carousel for Offers component[TKT104]

--- src/components/layouts/Offers/Offers.jsx
+++ src/components/layouts/Offers/Offers.jsx
@@ -1,4 +1,32 @@
-import { Carousel } from 'react-bootstrap';
+// import { Carousel } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import OffersCardJob from '../../UI/Cards/OffersCardJob';
+import VerticalCarousel from '../../UI/VerticalCarousel/VerticalCarousel'
+import data from '../../UI/VerticalCarousel/data.json';
+
+import './offers.css';
+
+// import offerIconImg from '../../../assets/icons/svg/offer-programmer-icon.svg'
+
+const Offers = () => (
+  <>
+    <section>
+      <div className="row center-xs offers">
+        <div className="row center-xs offers-title">
+          <h2 className="col-xs-12 offers-h2">Ofertas</h2>
+        </div>
+        <div className="row center-xs ">
+          <div className="col-xs-12">
+            <VerticalCarousel data={data.slides} leadingText={data.leadingText} />
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
+);
+export default Offers;
+/* import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import OffersCardJob from '../../UI/Cards/OffersCardJob';
@@ -46,18 +74,4 @@ const Offers = () => (
     </section>
   </>
 );
-export default Offers;
-
-/*  <div className="row center-xs">
-        <h1>Ofertas</h1>
-        <div className="col-xs-12">
-          <Carousel className="carousel vert slide">
-            <Carousel.Item className="carousel-item">
-              <img src={image1} alt="" />
-            </Carousel.Item>
-            <Carousel.Item className="carousel-item">
-              <img src={image2} alt="" />
-            </Carousel.Item>
-          </Carousel>
-        </div>
-      </div> */
+export default Offers; */
