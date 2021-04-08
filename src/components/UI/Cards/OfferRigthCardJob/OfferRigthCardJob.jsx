81a243d87dfc598ac2bcfa6ae2ab5cf580b75786
DaviD D <jdavid.martinez@gmail.com>
2021-04-08 10:41:54 -0500

    Infra: styling  Rigth Card's Offers component[TKT104]

--- src/components/UI/Cards/OfferRigthCardJob/OfferRigthCardJob.jsx
+++ src/components/UI/Cards/OfferRigthCardJob/OfferRigthCardJob.jsx
@@ -18,17 +18,47 @@ const OfferRigthCardJob = (
             ¡Te estamos Buscando!
           </Card.Title>
         </div>
-        <div>
-          <Card.Title className="">
-            Se busca
-            {tittleOffer}
-          </Card.Title>
-        </div>
-        <div className="offer-rigth-card-text">
-          ¡Buscamos!
-          {tittleOffer}
-          en
-          {skillOffer}
+        <div className="offer-rigth-card-main-info">
+          <div className="row">
+            <Card.Title className="col-xs-6">
+              Se busca
+              {tittleOffer}
+            </Card.Title>
+          </div>
+          <div className="row offer-rigth-card-text">
+            <Card.Text className="col-xs-10">
+              ¡Buscamos!
+              {tittleOffer}
+              en
+              {skillOffer}
+            </Card.Text>
+          </div>
+          <div>
+            <div className="row offer-rigth-card-skills">
+              <Card.Text className="col-xs-6">
+                Skils desables:
+              </Card.Text>
+              <Card.Text className="col-xs-6">
+                Java(Springboot),Python
+              </Card.Text>
+            </div>
+            <div className="row offer-rigth-card-skills">
+              <Card.Text className="col-xs-6">
+                Experiencia:
+              </Card.Text>
+              <Card.Text className="col-xs-6">
+                3 años
+              </Card.Text>
+            </div>
+          </div>
+          <div className="row ubication">
+            <Card.Text className="col-xs-4">
+              Cali, Armenia, Medellin, Bogota
+            </Card.Text>
+            <Card.Text className="col-xs-8">
+              Empleapp
+            </Card.Text>
+          </div>
         </div>
       </Card.Body>
     </Card>
