f81d6c9d192041f9142f796bfde0b81ae39d2a20
DaviD D <jdavid.martinez@gmail.com>
2021-04-16 11:16:43 -0500

    INFRA: working on responsive disigne on modules Features [TKT127]

--- src/components/layouts/AboutUs/AboutUs.jsx
+++ src/components/layouts/AboutUs/AboutUs.jsx
@@ -11,12 +11,12 @@ const AboutUs = () => (
 
       <div className="row center-xs">
         <div className="row about-us">
-          <div className="col-xs-12 col-sm-6 col-lg-5">
+          <div className="col-xs-12 col-sm-12 col-lg-5">
             <p className="about-us-text">
               EmpleApp es una empresa que Conecta perfiles potenciales de una manera rápida,
               ágil y efectiva, enfocando su metodología en las personas,
             </p>
-            <p className="about-us-text">
+            <p className="about-us-text second-text">
               potencializando el perfil laboral e incrementando la visibilidad
               ante los reclutadores/buscadores de empleo y facilita para las
               empresas la consecución de personal idóneo en menor tiempo.
