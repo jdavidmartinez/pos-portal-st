77efc50eac23078788fa70da1ae66db753261831
DaviD D <jdavid.martinez@gmail.com>
2021-04-19 19:38:42 -0500

    INFRA: creating vertical carousel using react-elastic-carousel [TKT127]

--- src/components/layouts/OffersCarouselHorizontal/OffersCarouselHorizontal.jsx
+++ src/components/layouts/OffersCarouselHorizontal/OffersCarouselHorizontal.jsx
@@ -1,73 +1,24 @@
-/* Offers Layaout-component */
+import { Component } from 'react';
+import Carousel from 'react-elastic-carousel';
 
-import Carousel from 'react-bootstrap/Carousel';
+class OffersCarouselHorizontal extends Component {
+  state = { // eslint-disable-line
+    items: [
+      { id: 1, title: 'item #1' },
+      { id: 2, title: 'item #2' },
+      { id: 3, title: 'item #3' },
+      { id: 4, title: 'item #4' },
+      { id: 5, title: 'item #5' }
+    ]
+  }
 
-import './offers-carousel-horizontal.css'
-
-import image1 from '../../../assets/img/companies/avt-ingenieria.png'
-
-const OffersCarouselHorizontal = () => { // eslint-disable-line
-  return (
-    <>
-      <div className="offers-carousel-horizontal">
-        <Carousel>
-          <Carousel.Item interval={1000} className="carousel-item">
-            <h3>Test</h3>
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img
-              className="d-block w-100"
-              src={image1}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
-    </>
-  );
+  render() {
+    const { items } = this.state;
+    return (
+      <Carousel>
+        {items.map((item) => <div key={item.id}>{item.title}</div>)}
+      </Carousel>
+    )
+  }
 }
-
 export default OffersCarouselHorizontal;
-/* return(
-      <>
-    <Carousel>
-    <Carousel.Item interval={1000}>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item interval={500}>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="Second slide"
-      />
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
-      />
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
- </> );
- */
