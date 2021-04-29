43c71432ca3e9def0b73f08ff8a84933762c7797
DaviD D <jdavid.martinez@gmail.com>
2021-04-29 08:53:49 -0500

    INFRA:V1.6 creating responsive carousel for offers component using react-slike carousel[TKT127]

--- src/components/layouts/Offers/Offers.jsx
+++ src/components/layouts/Offers/Offers.jsx
@@ -8,12 +8,12 @@ const Offers = () => (
   <>
     <section>
       <div className="offers">
-        <div className="row center-xs offers-title">
-          <h2 className="col-xs-12 col-lg-12 h2-titles">Ofertas de Empleo</h2>
+        <div className="row">
+          <h2 className="col-lg-12 offers-title h2-titles">Ofertas de Empleo</h2>
         </div>
         <div className="row">
-          <div className="col-xs-12 col-lg-12 center-xs offer-carousel">
-            <VerticalCarousel data={data.slides} leadingText={data.leadingText} />
+          <div className="col-lg-12 offer-carousel">
+            <VerticalCarousel data={data.slides} />
           </div>
         </div>
       </div>
