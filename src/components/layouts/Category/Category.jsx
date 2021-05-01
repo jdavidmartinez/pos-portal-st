23eb1866033f558e3c143d272976a35835883818
DaviD D <jdavid.martinez@gmail.com>
2021-05-01 15:42:36 -0500

    V1.5 layout frontend POS Portal ST

--- src/components/layouts/Category/Category.jsx
+++ src/components/layouts/Category/Category.jsx
@@ -189,37 +189,3 @@ function Category() {
 }
 
 export default Category;
-/*
-function Category() {
-  const [quantity, setTitle] = useState('');  // eslint-disable-line
-
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1
-  };
-  return (
-    <div className="simple-slider">
-      <Slider {...settings}>
-      {data.slides.map((item) => (
-        <div key={item.id}>
-          <CategoryCard
-            tittleOffer={item.title}
-            companie={item.companie}
-            cost={item.cost}
-            description={item.description}
-            offerIcon={offerIcon}
-            offerVactNumber={item.offerVactNumber}
-            offerNumber={item.offerNumber}
-          />
-        </div>
-      ))}
-    </Slider>
-  </div>
-);
-}
-
-export default Category;
-*/
